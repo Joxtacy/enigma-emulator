7d1947d68decdd57e4769842caad24f73b47d617
Jesper Hasselquist <psy08jh@gmail.com>
2018-12-20 21:45:34 +0100

    Did some webpack setup and stuff

--- app/app.js
+++
@@ -1,3 +0,0 @@
-import { EnigmaM3 } from './wheels/wheels';
-
-console.log(EnigmaM3);
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,48 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html'
+});
 
 module.exports = {
-    entry: './app/app.js',
+    entry: './src/app.js',
     output: {
         filename: 'app-bundle.js',
-        path: path.resolve(__dirname, 'build')
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
-                test: /\.m?.js$/,
-                exclude: /(node_modules)/,
+                test: /\.js$/,
+                exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: ['@babel/preset-env', '@babel/preset-react']
                     }
                 }
             },
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: '[name]_[local]_[hash:base64]',
+                            sourceMap: true,
+                            minimize: true
+                        }
+                    }
                 ]
             }
         ]
-    }
+    },
+    plugins: [htmlPlugin]
 };
